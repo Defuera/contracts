beb0e6a9325c854d597b907da4a7396d988ceb62
defuera <denis@devartel.io>
2023-09-26 22:13:59 +0100

    Fix

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,22 +1,11 @@
-import { ethers } from "hardhat";
+import { ethers } from 'hardhat';
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  const nft = await ethers.deployContract('ArtistPoolformation');
 
-  const lockedAmount = ethers.parseEther("0.001");
+  await nft.waitForDeployment();
 
-  const lock = await ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
-
-  await lock.waitForDeployment();
-
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+  console.log('NFT Contract Deployed at ' + nft.target);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
